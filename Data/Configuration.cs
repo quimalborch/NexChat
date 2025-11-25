8e6ffa8173a12ff6a3375afbc70d08180ff6bea3
aleixmarti <aleixmartiblanco@gmail.com>
2025-11-25 18:12:58 +0100

    Refactorización: mover clase Configuration a nuevo archivo
    Se eliminó la clase `Configuration` del archivo `Usuario.cs` y se trasladó a un nuevo archivo `Configuration.cs`.
    Se añadió la propiedad `paletaColoresSeleccionada` con un valor predeterminado de `PaletaColoresSeleccionada.Automatico`.
    Se incorporó un nuevo enumerador `PaletaColoresSeleccionada` con opciones de paletas de colores (`Automatico`, `Oscuro`, `Claro`, `Rojo`, `Verde`, `Morado`).
    Se mantuvieron las propiedades `idUsuario` y `nombreUsuario`, así como el constructor que inicializa `idUsuario` con un GUID.

--- Data/Configuration.cs
+++ Data/Configuration.cs
@@ -9,12 +9,22 @@ namespace NexChat.Data
     public class Configuration
     {
         public string idUsuario { get; set; }
-
         public string nombreUsuario { get; set; } = "Yo";
+        public PaletaColoresSeleccionada paletaColoresSeleccionada { get; set; } = PaletaColoresSeleccionada.Automatico;
 
         public Configuration() 
         {
             idUsuario = Guid.NewGuid().ToString();
+        }
+
+        public enum PaletaColoresSeleccionada
+        {
+            Automatico,
+            Oscuro,
+            Claro,
+            Rojo,
+            Verde,
+            Morado
         }
     }
 }
